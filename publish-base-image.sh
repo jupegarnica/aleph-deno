d53782f2c8e8e9d589dfdde72078c4d1414b64ae
Jupegarnica <juan@geekshubs.com>
2021-03-01 16:02:46 +0100

    add more secrets

--- publish-base-image.sh
+++ publish-base-image.sh
@@ -2,7 +2,7 @@
 
 docker build -t aleph-base -f Dockerfile.base .
 
-cat ./github-token | docker login https://docker.pkg.github.com -u jupegarnica --password-stdin
+cat ./github-token.secret | docker login https://docker.pkg.github.com -u jupegarnica --password-stdin
 
 docker tag aleph-base docker.pkg.github.com/jupegarnica/aleph-deno/aleph-base:latest
 docker push docker.pkg.github.com/jupegarnica/aleph-deno/aleph-base:latest
