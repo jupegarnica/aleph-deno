1595fb2eef3691da40cf02d3e3837c2b5f192720
Jupegarnica <juan@geekshubs.com>
2021-03-03 12:19:36 +0100

    testing new todo patern

--- app/app.tsx
+++ app/app.tsx
@@ -1,10 +1,10 @@
 import type { ComponentType } from 'react'
 import React from 'react'
-// TODO this works?
 export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
   return (
     <main>
       <head>
+        {/* TODO this works? */}
         <link rel="shortcut icon" type="image/svg" href="/favicon.svg"/>
         <link rel="stylesheet" href="./style/index.css" />
         <title>Jupegarnica</title>
