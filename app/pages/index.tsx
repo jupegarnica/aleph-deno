359e03f42c00f3de9689403297f21649e7bb51ec
Jupegarnica <juan@geekshubs.com>
2021-03-03 12:43:15 +0100

    test new patern

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Typist from 'https://esm.sh/react-typist';
+// TODO works?
 export default function Home() {
   return (
     <div className='page'>
       <Typist>
+        {/* TODO HOLA? [TEST]*/}
         Animate this text.
       </Typist>
     </div>
