d0e41425680744b2fda8cbda45362c0022c72468
gh-pages <>
2021-03-04 08:03:28 +0000

    update docs

---
+++ docs/_aleph/pages/index.bundle.02074e7ab.js
@@ -0,0 +1,1 @@
+(()=>{var e,t,a=(e=__ALEPH.pack["https://esm.sh/react@17.0.1"].default,t=__ALEPH.pack["https://esm.sh/react-typist"].default,{default:function(){return e.createElement("div",{className:"page"},e.createElement(t,null,"Animate this text."))}});__ALEPH.pack["/pages/index.tsx"]=a})();
