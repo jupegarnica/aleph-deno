22ae2cbd3f6c9b4a4b12562ab172b94cb82ead31
Jupegarnica <juan@geekshubs.com>
2021-03-05 08:37:38 +0100

    add baseUrl

--- app/_aleph.config.ts
+++
@@ -1,7 +0,0 @@
-import sassLoader from 'https://deno.land/x/aleph@v0.3.0-alpha.8/plugins/sass.ts'
-
-import type { Config } from 'https://deno.land/x/aleph@v0.3.0-alpha.8/types.ts'
-
-export default (): Config => ({
-  plugins: [sassLoader()]
-})
