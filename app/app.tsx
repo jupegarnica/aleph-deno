3fd4ac0a1bf369f92c90417efcdfde37da698aed
Jupegarnica <juan@geekshubs.com>
2021-03-07 10:29:51 +0100

    add viweport responsive

--- app/app.tsx
+++ app/app.tsx
@@ -38,6 +38,7 @@ export default function App({
           type='image/svg'
           href='favicon.svg'
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         {/* <link rel="stylesheet" href="style/index.css" /> */}
         <title>GARN</title>
       </head>
