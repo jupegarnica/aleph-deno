64a8dcdf8e8523fd72a85f11ebef3204383fcd19
Jupegarnica <juan@geekshubs.com>
2021-03-07 11:16:58 +0100

    fix publish base image

--- scripts/publish-base-image.sh
+++ scripts/publish-base-image.sh
@@ -3,7 +3,7 @@ DIR=$(dirname $(realpath "$0"))
 
 docker build -t aleph-base -f Dockerfile.base $DIR &&\
 
-cat $DIR/github-token.secret | docker login https://docker.pkg.github.com -u jupegarnica --password-stdin &&\
+cat $DIR/../github-token.secret | docker login https://docker.pkg.github.com -u jupegarnica --password-stdin &&\
 
 docker tag aleph-base docker.pkg.github.com/jupegarnica/aleph-deno/aleph-base:latest &&\
 docker push docker.pkg.github.com/jupegarnica/aleph-deno/aleph-base:latest
