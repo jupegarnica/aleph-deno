3ea9377c9f6e1c6d22adace380d06e4cf9ccaaf7
Jupegarnica <juan@geekshubs.com>
2021-03-13 16:45:00 +0100

    change import aleph from master

--- app/SSG.aleph.config.ts
+++ app/SSG.aleph.config.ts
@@ -1,6 +1,6 @@
 // import sassLoader from 'https://deno.land/x/aleph@v0.3.0-alpha.8/plugins/sass.ts'
 
-import type { Config } from 'https://deno.land/x/aleph@v0.3.0-alpha.8/types.ts'
+import type { Config } from 'aleph/types.ts'
 
 export default (): Config => ({
   // plugins: [sassLoader()]
--- app/aleph.config.ts
+++ app/aleph.config.ts
@@ -1,10 +1,9 @@
 // import sassLoader from 'https://deno.land/x/aleph@v0.3.0-alpha.8/plugins/sass.ts'
 
-import type { Config } from 'https://deno.land/x/aleph@v0.3.0-alpha.8/types.ts'
+import type { Config } from 'https://raw.githubusercontent.com/alephjs/aleph.js/master/types.ts'
 
 export default (): Config => ({
   // plugins: [sassLoader()]
   // baseUrl: 'aleph-deno',
   // ssr:false
-  postcss: undefined,
 })
