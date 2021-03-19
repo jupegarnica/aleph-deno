f72eb9b400492b5e51fed244706e272157e2db00
Jupegarnica <juan@geekshubs.com>
2021-03-19 18:19:11 +0100

    clean up

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -4,4 +4,17 @@ VERSION=$1;
 
 docker pull docker.pkg.github.com/jupegarnica/aleph-deno/aleph-prod:$VERSION;
 
-docker run --rm -p 8081:8080 docker.pkg.github.com/jupegarnica/aleph-deno/aleph-prod:$VERSION;
+docker run --rm -p 8080:8080 docker.pkg.github.com/jupegarnica/aleph-deno/aleph-prod:$VERSION
+
+
+# http://www.gnu.org/software/coreutils/manual/html_node/timeout-invocation.html
+# timeout 5
+# if [ $? -eq 124 ]; then
+#    echo "failed"
+#    exit 1
+# else
+#     # No hang
+#     echo 'OK'
+#     echo $?
+#     exit 0
+# fi
